10ed99ca846edcaf9748ea31e6a99f1d45dc7d62
Muhammad Sajjad <sajjad786pak@hotmail.com>
2023-06-02 11:08:52 +0100

    2nd Commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+Maths objMath = new Maths();
+int sum = objMath.Add(3, 5);
 Console.WriteLine("Hello, World!");
+Console.WriteLine($"Sum={sum}");
+Console.WriteLine("Done.");
