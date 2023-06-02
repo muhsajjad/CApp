e808114b90fdcbe3e0448c81761c0165a72d5278
Muhammad Sajjad <sajjad786pak@hotmail.com>
2023-06-02 12:42:25 +0100

    Binary Search and Factorial

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Searches objSearch = new Searches();
+int[] iArray = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 };
+int key = 2222;
+var outPut = objSearch.BinarySearch(iArray, key);
+Console.WriteLine($"output value = {outPut}");
+
+int numberFact = 5;
+var fact = objSearch.Factorial(numberFact);
+Console.WriteLine($"Factorial of {numberFact} =  {fact.ToString()}");
+
+Console.ReadLine();
+
 Maths objMath = new Maths();
 int sum = objMath.Add(3, 5);
 Console.WriteLine("Hello, World!");
 Console.WriteLine($"Sum={sum}");
 Console.WriteLine("Done.");
+int[] inputArray = { 1, 2, 3, 4, 5, 6, 7, 7, 8 };
---
+++ Searches.cs
@@ -0,0 +1,80 @@
+﻿public class Searches
+{
+    public object BinarySearch(int[] inputArray, int key)
+    {
+        var outValue = BinarySearchAlgo1(inputArray, key);
+        return outValue;
+    }
+    //public object BinarySearch2Recursive(int[] inputArray, int key, int min, int max)
+    //{
+    //    var outValue = BinarySearchAlgo2Recursive(inputArray, key, min, max);
+    //    return outValue;
+    //}
+
+    private object BinarySearchAlgo1(int[] iArray, int key)
+    {
+        int min = 0;
+        int max = iArray.Length - 1;
+        while (min <= max)
+        {
+            int mid = (min + max) / 2;
+            if (key == iArray[mid])
+            {
+                return ++mid;
+            }
+            else if (key < iArray[mid])
+            {
+                max = mid - 1;
+            }
+            else
+            {
+                min = mid + 1;
+            }
+        }
+        return "Nil";
+    }
+    //private object BinarySearchAlgo2Recursive(int[] iArray, int key, int min, int max)
+    //{
+
+    //}
+
+    public void FizzBuzz()
+    {
+        int[] myNum = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 };
+
+        for (int i = 0; i < myNum.Length; i++)
+        {
+            string str = "";
+            if (myNum[i] % 3 == 0 && myNum[i] % 5 == 0)
+            {
+                str += "FizzBuzz";
+            }
+            else if (myNum[i] % 3 == 0)
+            {
+                str += "Fizz";
+            }
+            else if (myNum[i] % 5 == 0)
+            {
+                str += "Buzz";
+            }
+            else if (str.Length == 0)
+            {
+                str = i.ToString();
+            }
+            Console.WriteLine(str);
+        }
+    }
+
+    public int Factorial(int num)
+    {
+        if (num == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            return num * Factorial(num - 1);
+        }
+    }
+
+}
