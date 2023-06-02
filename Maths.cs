b561833b0ab4ebb6de80c69e766329bd9bf9e88f
Muhammad Sajjad <sajjad786pak@hotmail.com>
2023-06-02 11:04:28 +0100

    Add project files.

---
+++ Maths.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Maths
+{
+    public Maths()
+    {
+
+    }
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+    public int Divide(int x, int y)
+    {
+        return (int)(x / y);
+    }
+}
